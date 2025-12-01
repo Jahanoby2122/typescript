07a07b5b957c003d45d32943ac898c7dd7247636
Akashahmed2122 <jahanoby2122@gmail.com>
2025-12-01 20:11:29 +0600

    succesfully complete genericinterfafce

--- genericswithinterface.ts
+++ genericswithinterface.ts
@@ -1,24 +1,54 @@
-interface Developer {
+interface Developer <t> {
     name:string,
     salary:number,
     device:{
         brand: string,
         model: string,
         releseYear: string
-    }
+    };
+    smartWatch: t
     
+}
+
+type Smartwatchtype = {
+    heartRate: string;
+    stopWatch: boolean
 }
 
 
 
 
-
-const poorDeveloper: Developer = {
+const poorDeveloper: Developer<Smartwatchtype> = {
     name: 'John Doe',
     salary: 50000,
     device: {
         brand: 'Dell',
         model: 'Inspiron 15',
         releseYear: '2018'
+    },
+    smartWatch:{
+        heartRate: '80 bpm',
+        stopWatch: false
+    }
+}
+
+
+type Smartwatchtypereach = {
+    heartRate: string;
+   
+}
+
+
+const reachDeveloper: Developer<Smartwatchtypereach> = {
+    name: ' Doe',
+    salary: 10000,
+    device: {
+        brand: 'Lenivo',
+        model: 'Inspiron 15',
+        releseYear: '2010'
+    },
+    smartWatch:{
+        heartRate: '880 bpm',
+       
     }
 }
